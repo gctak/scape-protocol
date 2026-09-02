72ed39018bae79575b4ca82256668db5ac5bc278
gctak <g.takingami@hotmail.com>
2026-09-02 16:57:03 -0300

    feat: adiciona animação de abaixar do personagem ao pressionar seta para baixo

--- js/game.js
+++ js/game.js
@@ -5,18 +5,24 @@ const originalLeft = character.style.left || getComputedStyle(character).left;
 const originalWidth =
   character.style.width || getComputedStyle(character).width;
 const instruction = document.querySelector(".game-instruction");
-const walkSprites = [
+const stopPosition = 15;
+const ground = document.querySelector(".game-ground");
+const normalWalkSprites = [
   "./assets/images/characters/FalkronWalkingA.png",
   "./assets/images/characters/FalkronWalkingB.png",
 ];
-const stopPosition = 15;
-const ground = document.querySelector(".game-ground");
+
+const downSprites = [
+  "./assets/images/characters/FalkronDownA.png",
+  "./assets/images/characters/FalkronDownB.png",
+];
 let groundPosition = 0;
 let groundMoving = false;
 let jogoComecou = false;
 let walkFrame = 0;
 let characterPosition = 6;
 let isJumping = false;
+let isDown = false;
 
 function jump() {
   isJumping = true;
@@ -31,12 +37,37 @@ function jump() {
   }, 400);
 }
 
+function down() {
+  isDown = true;
+  walkSprites.splice(0, walkSprites.length);
+  walkSprites = [
+    "./assets/images/characters/FalkronDownA.png",
+    "./assets/images/characters/FalkronDownB.png",
+  ];
+  character.style.width = "clamp(240px, 9vw, 280px)";
+  walkFrame = (walkFrame + 1) % walkSprites.length;
+  character.src = walkSprites[walkFrame];
+
+  setTimeout(() => {
+    character.style.bottom = originalBottom;
+    character.style.width = originalWidth;
+    isDown = false;
+    walkSprites.splice(0, walkSprites.length);
+    walkSprites = [
+      "./assets/images/characters/FalkronWalkingA.png",
+      "./assets/images/characters/FalkronWalkingB.png",
+    ];
+  }, 400);
+}
+
 function walk() {
   setInterval(() => {
     if (!isJumping) {
-      walkFrame = (walkFrame + 1) % walkSprites.length;
-      character.src = walkSprites[walkFrame];
+      const sprites = isDown ? downSprites : normalWalkSprites;
+      walkFrame = (walkFrame + 1) % sprites.length;
+      character.src = sprites[walkFrame];
     }
+
     if (characterPosition < stopPosition) {
       characterPosition += 1;
       character.style.left = characterPosition + "%";
@@ -48,7 +79,6 @@ function walk() {
 }
 
 function moveGround() {
-  console.log("moverChao rodando", groundPosition);
   groundPosition -= 0.15;
   if (groundPosition <= -50) {
     groundPosition = 0;
@@ -68,5 +98,19 @@ document.addEventListener("keydown", (event) => {
       walk();
     }
     jump();
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowDown" && !event.repeat) {
+    isDown = true;
+    character.style.width = "clamp(240px, 9vw, 280px)";
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "ArrowDown") {
+    isDown = false;
+    character.style.width = originalWidth;
   }
 });
